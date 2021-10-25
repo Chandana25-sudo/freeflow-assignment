93c840017bba7f3a3f024dea84bc970db8839104
Chandana25_Sudo <chandana.velpula16@gmail.com>
2021-10-25 10:36:09 +0530

    ch

--- src/App.js
+++ src/App.js
@@ -9,25 +9,45 @@ import {Container,Row,Col} from 'react-bootstrap';
 function App() {
   return (
     <div className="App">
-      <table >
-         <div className="row">
-           {/* <Col md={2}> */}
-              <div className="col-1">
-                <Icons/>
-              </div>
-            {/* </Col> */}
-            {/* <Col md={6}>   */}
-              <div className="col-7">
-                <Scroll/>
-              </div>
-            {/* </Col> */}
-            {/* <Col md={4}>   */}
-              <div className="col-4">
-                <Aboutus/>
-              </div>
-            {/* </Col> */}
-          </div>
-        </table>  
+      <div className="desktop">
+        <table >
+          <div className="row">
+            {/* <Col md={2}> */}
+                <div className="col-1">
+                  <Icons/>
+                </div>
+              {/* </Col> */}
+              {/* <Col md={6}>   */}
+                <div className="col-7">
+                  <Scroll/>
+                </div>
+              {/* </Col> */}
+              {/* <Col md={4}>   */}
+                <div className="col-4">
+                  <Aboutus/>
+                </div>
+              {/* </Col> */}
+            </div>
+          </table> 
+        </div>   
+        <div className="mobile">
+        <table >
+          <div className="row">
+            {/* <Col md={2}> */}
+                <div className="col-sm-2">
+                  <Icons/>
+                </div>
+              {/* </Col> */}
+              {/* <Col md={6}>   */}
+                <div className="col-sm-10">
+                  <Scroll/>
+                </div>
+              {/* </Col> */}
+             
+            </div>
+          </table> 
+
+        </div>
     </div>
   );
 }
